fe6dd3bb101fef11f50c805c9bcc21f850f79e48
Tim Burke <tim.burke@gmail.com>
2017-05-08 10:05:22 -0400

    Clean up demo scripts a bit

--- boto3_demo.py
+++ boto3_demo.py
@@ -2,22 +2,22 @@
 from __future__ import print_function
 import io, boto3
 
-session = boto3.Session(profile_name='swiftstack')
+session = boto3.Session(profile_name='swiftstack-v4')
 s3 = session.resource('s3', endpoint_url='http://saio.dev')
-
-print(list(s3.buckets.all()))
+list(s3.buckets.all())
 
 bucket = s3.create_bucket(Bucket='test-bucket')
-print(list(s3.buckets.all()))
+list(s3.buckets.all())
 
-bucket.upload_fileobj(Key='test-object', Fileobj=io.BytesIO(b'Hello, world!'))
-print(list(bucket.objects.all()))
+bucket.upload_fileobj(Key='test-object', Fileobj=io.BytesIO(b'Hello, Boston!'))
+list(bucket.objects.all())
 
 key = bucket.Object('test-object')
-print(key.get()['Body'].read())
+key.get()['Body'].read()
 
-key.delete()
-print(list(bucket.objects.all()))
+for bucket in s3.buckets.all():
+    for key in bucket.objects.all():
+        key.delete()
+    bucket.delete()
 
-bucket.delete()
-print(list(s3.buckets.all()))
+list(s3.buckets.all())
--- boto_demo.py
+++ boto_demo.py
@@ -5,20 +5,19 @@ import boto, requests
 s3 = boto.connect_s3(profile_name='swiftstack', host='saio.dev')
 s3.auth_region_name = 'US'
 
-buckets = s3.get_all_buckets()
-print(buckets)
-
+s3.get_all_buckets()
 bucket = s3.create_bucket('test-bucket')
-print(s3.get_all_buckets())
+bucket
+s3.get_all_buckets()
+key = bucket.new_key('test-object')
+key
+key.set_contents_from_string('Hello, Boston!')
+bucket.get_all_keys()
+requests.get(s3.generate_url(3600, 'GET', 'test-bucket', 'test-object')).content
 
-key = bucket.new_key('object')
-key.set_contents_from_string('Hello, world!')
-print(bucket.get_all_keys())
+for bucket in s3.get_all_buckets():
+    for key in bucket.get_all_keys():
+        key.delete()
+    bucket.delete()
 
-print(requests.get(s3.generate_url(3600, 'GET', 'test-bucket', 'object')).content)
-
-for key in bucket.get_all_keys():
-    key.delete()
-
-bucket.delete()
-print(s3.get_all_buckets())
+s3.get_all_buckets()
