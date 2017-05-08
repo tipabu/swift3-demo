eda9a9f0ca15832cc4c108b5c2910bb5649e39c8
Tim Burke <tim.burke@gmail.com>
2017-05-08 00:38:16 -0400

    Add boto3 demo

---
+++ boto3_demo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from io import BytesIO
+
+import boto3
+
+session = boto3.Session(profile_name='swiftstack')
+s3 = session.resource('s3', endpoint_url='http://saio.dev')
+
+print(list(s3.buckets.all()))
+
+bucket = s3.create_bucket(Bucket='test-bucket')
+print(list(s3.buckets.all()))
+
+bucket.upload_fileobj(Key='test-object', Fileobj=BytesIO(b'Hello, world!'))
+print(list(bucket.objects.all()))
+
+key = bucket.Object('test-object')
+print(key.get()['Body'].read())
+
+key.delete()
+print(list(bucket.objects.all()))
+
+bucket.delete()
+print(list(s3.buckets.all()))
